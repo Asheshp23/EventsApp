0e28b98acc7f6b3443e3c09b5a8082283620a685
asheshp23 <asheshp23@gmail.com>
2024-12-02 10:52:13 -0500

    initial commit

---
+++ EventsApp/EventsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventsAppApp.swift
+//  EventsApp
+//
+//  Created by Ashesh Patel on 2024-12-02.
+//
+
+import SwiftUI
+
+@main
+struct EventsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
